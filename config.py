89542ca8818ddb87e96b4f9bf7878cc58cf86607
PiroLeader <158609590+PiroLeader@users.noreply.github.com>
2024-03-10 02:47:43 -0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    BOT_TOKEN = "6582971212:AAG_K_Iec_0nlI83g_Sq0Aq3DXwcv6Zs4lQ"
+    API_ID = "28485705"
+    API_HASH = "26d76803d487d6d0cc818cf191fa7518"
+    AUTH_USERS = '6211690957'
+
